bed1c7e0eb0640f83ea227eedbb5f67d79c92ce9
Roman Pushkarev <88341608+pushroman@users.noreply.github.com>
2025-02-14 17:06:52 +0300

    Initial Commit

---
+++ ScanNet/ScanNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanNetApp.swift
+//  ScanNet
+//
+//  Created by Роман Пушкарев on 14.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScanNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
